49fa9987ee49d18c3e9ae14d95f64e844be29f6b
lucaseunchae <lucaskim.dev@gmail.com>
2023-11-22 23:47:40 +0900

    feat: add base, common components
    (cherry picked from commit 64bfa4805da1b9cf8f4622e013ef8e16e539cc61)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import ConfirmDialog from '@/components/Common/ConfirmDialog.vue'
+import useConfirmDialogStore from '@/stores/useConfirmDialogStore'
+import { storeToRefs } from 'pinia'
 import { RouterView } from 'vue-router'
+
+/* Pinia */
+const { isShow: isShowConfirmDialog } = storeToRefs(useConfirmDialogStore())
 </script>
 
 <template>
@@ -10,6 +16,11 @@ import { RouterView } from 'vue-router'
       </Transition>
     </RouterView>
   </main>
+
+  <!-- Modal -->
+  <Teleport to="body">
+    <ConfirmDialog v-model:open="isShowConfirmDialog" />
+  </Teleport>
 </template>
 
 <style lang="scss">
