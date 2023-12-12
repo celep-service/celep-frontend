009a493adfc299459ac187b6916d1d94422637fb
lucaseunchae <lucaskim.dev@gmail.com>
2023-12-12 22:35:01 +0900

    feat: add showErrorAlertDialog action into alert dialog store

--- src/composables/useAlertDialogStore.ts
+++ src/composables/useAlertDialogStore.ts
@@ -1,3 +1,5 @@
+import type { ApiErrorResponse } from '@/model/Api'
+import axios from 'axios'
 import { defineStore } from 'pinia'
 import { reactive } from 'vue'
 
@@ -9,8 +11,14 @@ const useAlertDialogStore = defineStore('alertDialog', () => {
   const showAlertDialog = (body: string) => {
     dialogs.push(body)
   }
+  const showErrorAlertDialog = (error: unknown) => {
+    if (axios.isAxiosError<ApiErrorResponse>(error) && error.response) {
+      const { message, code } = error.response.data
+      dialogs.push(`[에러 코드: ${code}] ${message}`)
+    }
+  }
 
-  return { dialogs, showAlertDialog }
+  return { dialogs, showAlertDialog, showErrorAlertDialog }
 })
 
 export default useAlertDialogStore
