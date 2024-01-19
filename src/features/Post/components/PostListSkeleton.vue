e4ed6dfdfb7aedf5dc00dcfc1ee5e63bc8c8017d
Eunchae Kim <lucaskim.dev@gmail.com>
2024-01-20 01:49:49 +0900

    design: change PostListSkeleton ui

--- src/features/Post/components/PostListSkeleton.vue
+++ src/features/Post/components/PostListSkeleton.vue
@@ -15,7 +15,7 @@ const props = withDefaults(defineProps<Props>(), {})
         <BaseSkeleton shape="circle" width="24px" height="24px" />
         <BaseSkeleton width="80px" height="16px" />
       </div>
-      <BaseSkeleton height="200px" />
+      <BaseSkeleton class="post-list-skeleton__item__swiper" />
     </div>
   </div>
 </template>
@@ -28,12 +28,20 @@ const props = withDefaults(defineProps<Props>(), {})
   &__item {
     display: flex;
     flex-direction: column;
+    width: 100vw;
 
     &__user-info-wrapper {
       display: flex;
       align-items: center;
       gap: 10px;
       padding: 12px;
+    }
+
+    &__swiper {
+      width: 100%;
+      max-width: 400px;
+      aspect-ratio: 1;
+      margin: 0 auto;
     }
   }
 }
