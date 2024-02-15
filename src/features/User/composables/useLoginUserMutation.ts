1e7baba31252af3f2ca777a0a251ea8f9d9d1553
Eunchae Kim <lucaskim.dev@gmail.com>
2024-02-15 15:06:17 +0900

    feat: add invalidating all queries when logged in

--- src/features/User/composables/useLoginUserMutation.ts
+++ src/features/User/composables/useLoginUserMutation.ts
@@ -2,14 +2,17 @@ import { loginUser } from '@/api/user'
 import useAlertDialogStore from '@/composables/useAlertDialogStore'
 import { ACCESS_TOKEN_KEY } from '@/constants/jwt'
 import type { LoginUserRequest } from '@/model/User'
-import { useMutation } from '@tanstack/vue-query'
+import { useMutation, useQueryClient } from '@tanstack/vue-query'
 import type { AnyFn } from '@vueuse/core'
 
 const useLoginUserMutation = (onError: AnyFn) => {
+  const queryClient = useQueryClient()
+
   return useMutation({
     mutationFn: (req: LoginUserRequest) => loginUser(req),
     onSuccess: (res) => {
       localStorage.setItem(ACCESS_TOKEN_KEY, res.data.accessToken)
+      queryClient.invalidateQueries({ refetchType: 'all' })
     },
     onError: (error) => {
       onError(error)
